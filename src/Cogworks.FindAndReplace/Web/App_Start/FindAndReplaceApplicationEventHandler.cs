066683d352334c832dd649ad398cbca1c66dc2a0
Marcin Zajkowski <m.zajkowski@sicc.pl>
2017-03-17 01:51:30 +0100

    Small refactoring

--- src/Cogworks.FindAndReplace/Web/App_Start/FindAndReplaceApplicationEventHandler.cs
+++ src/Cogworks.FindAndReplace/Web/App_Start/FindAndReplaceApplicationEventHandler.cs
@@ -11,16 +11,17 @@ namespace Cogworks.FindAndReplace.Web.App_Start
             TreeControllerBase.MenuRendering += FindAndReplace_MenuRendering;
         }
 
-        private void FindAndReplace_MenuRendering(global::Umbraco.Web.Trees.TreeControllerBase sender, global::Umbraco.Web.Trees.MenuRenderingEventArgs e)
+        private static void FindAndReplace_MenuRendering(TreeControllerBase sender, MenuRenderingEventArgs e)
         {
-            if(sender.TreeAlias == "content")
-            {
-                var menuItem = new global::Umbraco.Web.Models.Trees.MenuItem("findAndReplace", "Find and Replace");
-                menuItem.AdditionalData.Add("actionView", "/App_Plugins/FindAndReplace/Views/findandreplace.html");
-                menuItem.AdditionalData.Add("contentId", e.NodeId);
-                menuItem.Icon = "axis-rotation-2";
-                e.Menu.Items.Insert(e.Menu.Items.Count, menuItem);
-            }
+            if (sender.TreeAlias != "content") return;
+
+            var menuItem = new Umbraco.Web.Models.Trees.MenuItem("findAndReplace", "Find and Replace");
+            menuItem.AdditionalData.Add("actionView", "/App_Plugins/FindAndReplace/Views/findandreplace.html");
+            menuItem.AdditionalData.Add("contentId", e.NodeId);
+            menuItem.Icon = "axis-rotation-2";
+            menuItem.SeperatorBefore = true;
+
+            e.Menu.Items.Insert(e.Menu.Items.Count, menuItem);
         }
     }
 }
